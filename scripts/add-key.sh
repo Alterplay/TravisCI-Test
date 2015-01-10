01a4a547967fa2afef88f6b5ab8088c9663be8d5
Serg Krivoblotsky <krivoblotsky@me.com>
2015-01-10 23:23:01 +0200

    Add script updated

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -21,7 +21,7 @@ security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keycha
 
 # Put the provisioning profile in place
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp "./scripts/profile/$PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./scripts/profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
 
 if [ ! -f "~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovision" ]; then
     echo "***************************"
