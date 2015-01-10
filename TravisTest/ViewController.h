f8561a13ca2c79064b901f1cd34467226c6760d6
Serg Krivoblotsky <krivoblotsky@me.com>
2015-01-10 17:08:27 +0200

    Files moved

---
+++ TravisTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravisTest
+//
+//  Created by Sergii Kryvoblotskyi on 1/10/15.
+//  Copyright (c) 2015 Sergii Kryvoblotskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
